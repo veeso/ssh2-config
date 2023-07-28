523cd3272cc4545a404549a5959ad9680be15a51
veeso <christian.visintin@veeso.dev>
2023-07-28 15:44:05 +0200

    added tests

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@
 #![doc(html_playground_url = "https://play.rust-lang.org")]
 
 use std::fs::File;
-use std::io::{BufRead, self, BufReader};
+use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
 use std::time::Duration;
 // -- modules
@@ -114,15 +114,19 @@ impl SshConfig {
     /// Parse ~/.ssh/config file and return parsed configuration or parser error
     pub fn parse_default_file(rules: ParseRule) -> SshParserResult<Self> {
         let ssh_folder = dirs::home_dir()
-            .ok_or_else(|| SshParserError::Io(io::Error::new(io::ErrorKind::NotFound, "Home folder not found")))?
+            .ok_or_else(|| {
+                SshParserError::Io(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "Home folder not found",
+                ))
+            })?
             .join(".ssh");
 
-        let mut reader = BufReader::new(File::open(ssh_folder.join("config"))
-            .map_err(SshParserError::Io)?);
+        let mut reader =
+            BufReader::new(File::open(ssh_folder.join("config")).map_err(SshParserError::Io)?);
 
         Self::default().parse(&mut reader, rules)
     }
-
 
     pub fn get_hosts(&self) -> &Vec<Host> {
         &self.hosts
@@ -142,6 +146,27 @@ mod test {
         assert_eq!(config.hosts.len(), 1);
         assert_eq!(config.default_params(), HostParams::default());
         assert_eq!(config.query("192.168.1.2"), HostParams::default());
+    }
+
+    #[test]
+    fn should_parse_default_config() {
+        assert!(SshConfig::parse_default_file(ParseRule::ALLOW_UNKNOWN_FIELDS).is_ok());
+    }
+
+    #[test]
+    fn should_parse_config() {
+        use std::fs::File;
+        use std::io::BufReader;
+        use std::path::Path;
+
+        let mut reader = BufReader::new(
+            File::open(Path::new("./assets/ssh.config"))
+                .expect("Could not open configuration file"),
+        );
+
+        assert!(SshConfig::default()
+            .parse(&mut reader, ParseRule::STRICT)
+            .is_ok());
     }
 
     #[test]
